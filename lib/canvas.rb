9fda3278e3a0228abc2dff363320956ded0657bb
Charlotte Chang <yellowspiderlove@gmail.com>
2014-06-23 14:20:32 -0400

    test canvas errors

--- lib/canvas.rb
+++ lib/canvas.rb
@@ -9,9 +9,23 @@ class Canvas
 
   def plot
     input_array = command.split(' ')
-    canvas_object = input_array[0]
+    canvas_object = input_array[0].upcase
+
+    if canvas_object!= 'C'
+      raise "Sorry I don't recognize that command. Please try again."
+    end
+
     width = input_array[1].to_i
     canvas_height = input_array[2].to_i
+
+    unless width.integer? && width > 0
+      raise "Sorry only positive numbers are accepted. Please try again."
+    end
+
+    unless canvas_height.integer? && canvas_height > 0
+      raise "Sorry only positive numbers are accepted. Please try again."
+    end
+
 
     canvas_width = width + 2
     plotted_canvas_width = '-' * canvas_width + "\n"
@@ -19,13 +33,4 @@ class Canvas
     plotted_canvas = plotted_canvas_width + ('|'+ (' ' * width) + '|' + "\n") * canvas_height
     plotted_canvas += plotted_canvas_width
   end
-
-
-
-
 end
-
-
-#create a canvas/make a canvas accessible to plot
-#we need to get a command/pass the command to plot
-#output the correct grid
--- spec/canvas_spec.rb
+++ spec/canvas_spec.rb
@@ -1,6 +1,5 @@
 require 'draw'
 require 'canvas'
-require 'stringio'
 
 describe Canvas do
   it "should return a canvas command that is passed to it" do
@@ -22,13 +21,20 @@ grid
 )
   end
 
-
-
-
-  it "should give an error if the incorrect command is given for creating a canvas" do
-    
+  it "should give an error if the incorrect letter is given for creating a canvas" do
+    canvas = Canvas.new("D 20 4")
+    expect { canvas.plot }.to raise_error("Sorry I don't recognize that command. Please try again.")
   end
 
+  it "should not give an error if the correct letter is given lower case" do
+    canvas = Canvas.new("c 20 4")
+    expect { canvas.plot }.not_to raise_error
+  end
+
+  it "should give an error if a nonpositive integer is given for creating a canvas" do
+    canvas = Canvas.new("C -20 4")
+    expect {canvas.plot }.to raise_error("Sorry only positive numbers are accepted. Please try again.")
+  end
 end
 
 # describe Quit do
