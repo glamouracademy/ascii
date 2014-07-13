6978c371867c2b78b01326a194bf6ef89947970b
Charlotte Chang <yellowspiderlove@gmail.com>
2014-07-12 20:39:20 -0400

    add check fill coordinate content

--- lib/fill.rb
+++ lib/fill.rb
@@ -19,21 +19,15 @@ class Fill < Plot
     @colour = input_array[3] 
   end
 
-  def get_coordinates
-    fill_coordinate = [x,y]
-    top_coordinate = [x,y-1]
-    right_coordinate = [x+1,y]
-    bottom_coordinate = [x,y+1]
-    left_coordinate = [x-1,y]
-    fill_point = [fill_coordinate, top_coordinate, right_coordinate, bottom_coordinate, left_coordinate]
-
-    fill_point.select do |coordinate|
-      x = coordinate[0]
-      y = coordinate[1]
-      Canvas.current.get_coordinate(x,y) == " " 
+  def get_content(fill_coordinate)
+    x = fill_coordinate[0]
+    y = fill_coordinate[1]
+    fill_content = Canvas.current.get_coordinate(x,y)
+    if fill_content == " "
+      true
+    else 
+      false
     end
-
-
   end
 
   def content
@@ -41,7 +35,16 @@ class Fill < Plot
   end
 end
 
-#take fill_point_array and check those coordinates to grid_coordinates
-#within those grid_coordinates, check what is contained
-#if nothing then fill
-#otherwise do not fill
+#take fill_coordinate and check if empty XXX
+#if not empty check for "colour" (and whether it matches what has been requested)
+#if colour does not match or if empty,fill with requested colour
+#if there was a "colour", then fill all coordinates with the same content with the replaced colour
+
+#add/move 1 coordinate up and repeat until reach border
+#once reach border, move back down to "passing" coordinate and move right 1 coordinate and repeat
+
+#once reach the original fill_coordinate add/move 1 coordinate down and repeat until reach border
+#once reach border, move back up to "passing" coordinate and move left 1 coordinate and repeat
+
+
+
--- spec/fill_spec.rb
+++ spec/fill_spec.rb
@@ -13,59 +13,78 @@ describe Fill do
       expect(fill_point.colour).to eq("o")
     end
 
-    it "should return the adjacent real coordinates of the given fill point" do
+    it "should check the contents of the fill point" do
       fill_point = Fill.new("B 2 2 o")
-      expect(fill_point.get_coordinates).to include([2,2],[2,1],[3,2],[2,3],[1,2])
+      content = fill_point.get_content([2,2])
+      expect(content).to eq(true)
     end
 
-    it "should plot x's for the adjacent coordinates of the given fill point" do
-      fill_point = Fill.new("B 2 2 x")
-      fill_point.plot(@canvas)
-      plotted_fill_point = @canvas.output
-      expect(plotted_fill_point).to eq(<<gridline
------
-| x |
-|xxx|
-| x |
------
-gridline
-)
-    end
 
-    it "should plot the colour for the adjacent coordinates of the given fill point" do
-      fill_point = Fill.new("B 2 2 o")
-      fill_point.plot(@canvas)
-      plotted_fill_point = @canvas.output
-      expect(plotted_fill_point).to eq(<<gridline
------
-| o |
-|ooo|
-| o |
------
-gridline
-)
-    end
 
-    it "should return only valid fill coordinates" do
-      fill_point = Fill.new("B 2 1 y")
-      expect(fill_point.get_coordinates).not_to include([2,0])
-    end
+#     it "should plot x's for the adjacent coordinates of the given fill point" do
+#       fill_point = Fill.new("B 2 2 x")
+#       fill_point.plot(@canvas)
+#       plotted_fill_point = @canvas.output
+#       expect(plotted_fill_point).to eq(<<gridline
+# -----
+# | x |
+# |xxx|
+# | x |
+# -----
+# gridline
+# )
+#     end
 
-    it "should not replace the canvas border" do
-      fill_point = Fill.new("B 2 1 p")
-      fill_point.plot(@canvas)
-      plotted_fill_point = @canvas.output
-      expect(plotted_fill_point).to eq(<<gridline
------
-|ppp|
-| p |
-|   |
------
-gridline
-)
-    end
-    
+#     it "should plot the colour for the adjacent coordinates of the given fill point" do
+#       fill_point = Fill.new("B 2 2 o")
+#       fill_point.plot(@canvas)
+#       plotted_fill_point = @canvas.output
+#       expect(plotted_fill_point).to eq(<<gridline
+# -----
+# | o |
+# |ooo|
+# | o |
+# -----
+# gridline
+# )
+#     end
+
+#     it "should return only valid fill coordinates" do
+#       fill_point = Fill.new("B 2 1 y")
+#       expect(fill_point.get_coordinates).not_to include([2,0])
+#     end
+
+#     it "should not replace the canvas border" do
+#       fill_point = Fill.new("B 2 1 p")
+#       fill_point.plot(@canvas)
+#       plotted_fill_point = @canvas.output
+#       expect(plotted_fill_point).to eq(<<gridline
+# -----
+# |ppp|
+# | p |
+# |   |
+# -----
+# gridline
+# )
+#     end    
   end
+
+#   it "should fill an area" do
+#     canvas = Canvas.new("C 5 5")
+#     fill_point = Fill.new("B 3 3 o")
+#     fill_point.plot(canvas)
+#     plotted_fill_point = canvas.output
+#     expect(plotted_fill_point).to eq(<<gridline
+# -------
+# |ooooo|
+# |ooooo|
+# |ooooo|
+# |ooooo|
+# |ooooo|
+# -------
+# gridline
+# )
+#     end
 
 
   it "should return an error if the fill point is outside the canvas"
