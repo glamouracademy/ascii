ebd7eb411ca00880d6c5a44a0b952014620d4abf
Charlotte Chang <yellowspiderlove@gmail.com>
2014-07-13 11:55:56 -0400

    refactor CLI

--- lib/ascii.rb
+++ lib/ascii.rb
@@ -3,56 +3,37 @@ require_relative 'line'
 require_relative 'rectangle'
 require_relative 'fill'
 
-puts "Welcome to ASCII fun time draw time. Please tell me your name."
-name = gets
-name = name.chomp
-puts "Hello #{name}. To start a new canvas, please type the letter 'C' followed by the width and height coordinates.  If you want to quit at any time, type 'Q'. (But why would you want to quit when you're having so much fun?)"
-
-canvas_input = gets
-canvas_input = canvas_input.chomp
+puts "Welcome to ASCII fun time draw time."
 
 begin
-  canvas = Canvas.new(canvas_input)
-  grid = canvas.output
-  puts "Awesome!  Here's your canvas!" + "\n" + grid
-rescue => e
-  puts e.message
-end
+  puts "Enter command:"
+  command = gets
+  command = command.chomp
+  begin
+    case command[0]
+    when "C"
+      canvas = Canvas.new(command)
+      grid = canvas.output
+      puts "Awesome!  Here's your canvas!" + "\n" + grid
+    when "L"
+      line = Line.new(command)
+      plot_line = line.plot(canvas)
+      plotted_line = canvas.output
+      puts "Fun! Here's your line!" + "\n" + plotted_line
+    when "R"
+      rectangle = Rectangle.new(command)
+      plot_rectangle = rectangle.plot(canvas)
+      plotted_rectangle = canvas.output
+      puts "Spectacular! Here's your rectangle!" + "\n" + plotted_rectangle
+    when "B"
+      fill = Fill.new(command)
+      plot_fill = fill.plot(canvas)
+      plotted_fill = canvas.output
+      puts "GOAL! Here's your fill!" + "\n" + plotted_fill
+    end
+  rescue => e
+    puts e.message
+  end
+end until command == 'Q' || command == 'q'
 
-puts "Now please type the coordinates for a line, starting with the letter 'L' and followed by x1 y1 x2 y2."
-
-line_input = gets
-line_input = line_input.chomp
-
-begin
-  line = Line.new(line_input)
-  plot_line = line.plot(canvas)
-  plotted_line = canvas.output
-  puts "Here's your line!" + "\n" + plotted_line
-rescue => e
-  puts e.message
-end
-
-puts "Great! Please type the coordinates for a rectangle, starting with the letter 'R' and followed by x1 y1 x2 y2."
-
-rectangle_input = gets
-rectangle_input = rectangle_input.chomp
-
-begin
-  rectangle = Rectangle.new(rectangle_input)
-  plot_rectangle = rectangle.plot(canvas)
-  plotted_rectangle = canvas.output
-  puts "Here's your rectangle!" + "\n" + plotted_rectangle
-rescue => e
-  puts e.message
-end
-
-puts "Great! Please type the coordinates for a fill, starting with the letter 'B' and followed by x1 y1 colour."
-fill_input = gets
-fill_input = fill_input.chomp
-
-fill = Fill.new(fill_input)
-plot_fill = fill.plot(canvas)
-plotted_fill = canvas.output
-puts "Here's your fill!" + "\n" + plotted_fill
 
